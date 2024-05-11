6c03853e802b4f6f8b6f0bbbaa3e05111f6cecde
sayeedHossain191 <sayeed.hossain.sagor@gmail.com>
2024-05-11 08:35:37 +0600

    third commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,49 +1,69 @@
-
+import { useContext } from "react"
+import { AuthContext } from "../provider/AuthProvider"
+import { Link } from "react-router-dom"
 
 const Navbar = () => {
+
+    const { user } = useContext(AuthContext)
+
     return (
-        <div>
-            <div className="navbar bg-base-100">
-                <div className="navbar-start">
-                    <div className="dropdown">
-                        <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
-                        </div>
-                        <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-                            <li><a>Item 1</a></li>
-                            <li>
-                                <a>Parent</a>
-                                <ul className="p-2">
-                                    <li><a>Submenu 1</a></li>
-                                    <li><a>Submenu 2</a></li>
-                                </ul>
-                            </li>
-                            <li><a>Item 3</a></li>
-                        </ul>
-                    </div>
-                    <a className="btn btn-ghost text-xl">daisyUI</a>
-                </div>
-                <div className="navbar-center hidden lg:flex">
-                    <ul className="menu menu-horizontal px-1">
-                        <li><a>Item 1</a></li>
-                        <li>
-                            <details>
-                                <summary>Parent</summary>
-                                <ul className="p-2">
-                                    <li><a>Submenu 1</a></li>
-                                    <li><a>Submenu 2</a></li>
-                                </ul>
-                            </details>
-                        </li>
-                        <li><a>Item 3</a></li>
-                    </ul>
-                </div>
-                <div className="navbar-end">
-                    <a className="btn">Button</a>
+        <div className='navbar bg-base-100 shadow-sm container px-4 mx-auto'>
+            <div className='flex-1'>
+                <div className='flex gap-2 items-center'>
+                    <img className='w-auto h-7' src='' alt='' />
+                    <span className='font-bold'>SoloSphere</span>
                 </div>
             </div>
-        </div>
-    );
-};
+            <div className='flex-none'>
+                <ul className='menu menu-horizontal px-1'>
+                    <li>
+                        <div>Home</div>
+                    </li>
 
-export default Navbar;
+                    {!user && <li>
+                        <Link to='/login'>Login</Link>
+                    </li>}
+                </ul>
+
+                {user &&
+                    <div className='dropdown dropdown-end z-50'>
+                        <div
+                            tabIndex={0}
+                            role='button'
+                            className='btn btn-ghost btn-circle avatar'
+                        >
+                            <div className='w-10 rounded-full' title=''>
+                                <img
+                                    referrerPolicy='no-referrer'
+                                    alt='User Profile Photo'
+                                    src=''
+                                />
+                            </div>
+                        </div>
+                        <ul
+                            tabIndex={0}
+                            className='menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52'
+                        >
+                            <li>
+                                <div className='justify-between'>Add Job</div>
+                            </li>
+                            <li>
+                                <div>My Posted Jobs</div>
+                            </li>
+                            <li>
+                                <div>My Bids</div>
+                            </li>
+                            <li>
+                                <div>Bid Requests</div>
+                            </li>
+                            <li className='mt-2'>
+                                <button className='bg-gray-200 block text-center'>Logout</button>
+                            </li>
+                        </ul>
+                    </div>}
+            </div>
+        </div>
+    )
+}
+
+export default Navbar
